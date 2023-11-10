46baa06c34f277c9e7b2bd9bb2f50edb22041547
marcobusacca <marcobusacca01@gmail.com>
2023-11-10 11:20:29 +0100

    Snack-1 completato

--- src/org/lessons/java/Snack1.java
+++ src/org/lessons/java/Snack1.java
@@ -1,8 +1,26 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class Snack1 {
 
 	public static void main(String[] args) {
 		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Inserisci un numero: ");
+		String strValue = in.nextLine();
+		int intValue = Integer.valueOf(strValue);
+		
+		if (intValue % 2 == 0) {
+			
+			System.out.println(intValue);
+			
+		} else {
+			
+			System.out.println(intValue + 1);
+		}
+		
+		in.close();
 	}
 }
